ced1763164edab477b4f7562e8c91b806d1d139c
Putrifisichella <113339249+Putrifisichella@users.noreply.github.com>
2022-10-06 13:52:28 +0700

    tugas 1

---
+++ tugascpp01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+  int main(){
+  cout << "NPM       : 2210631170096 " << endl;
+  cout << "Nama      : Putri Fisichella Chasannove " << endl;
+  cout << "Umur      : 18 tahun 11 bulan " << endl;
+  cout << "Kelas     : 1B Informatika " << endl;
+  cout << "Hobi      : menonton film " << endl;
+  cout << "Cita-Cita : masuk surga " << endl;
+
+ return 0;
+}
